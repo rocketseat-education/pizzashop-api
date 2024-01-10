e96d37f5464f0966a227d65af3ef508bce1742d1
Diego Fernandes <diego.schell.f@gmail.com>
2024-01-10 09:17:42 -0300

    fix: disable email sending to test api without resend

--- src/http/routes/send-authentication-link.ts
+++ src/http/routes/send-authentication-link.ts
@@ -2,14 +2,14 @@ import Elysia, { t } from 'elysia'
 import { db } from '@/db/connection'
 import { authLinks } from '@/db/schema'
 import { createId } from '@paralleldrive/cuid2'
-import { resend } from '@/mail/client'
-import { AuthenticationMagicLinkTemplate } from '@/mail/templates/authentication-magic-link'
+// import { resend } from '@/mail/client'
+// import { AuthenticationMagicLinkTemplate } from '@/mail/templates/authentication-magic-link'
 import { env } from '@/env'
 import { UnauthorizedError } from './errors/unauthorized-error'
 
 export const sendAuthenticationLink = new Elysia().post(
   '/authenticate',
-  async ({ body, set }) => {
+  async ({ body }) => {
     const { email } = body
 
     const userFromEmail = await db.query.users.findFirst({
@@ -33,15 +33,17 @@ export const sendAuthenticationLink = new Elysia().post(
     authLink.searchParams.set('code', authLinkCode)
     authLink.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
 
-    await resend.emails.send({
-      from: 'Pizza Shop <naoresponda@fala.dev>',
-      to: email,
-      subject: '[Pizza Shop] Link para login',
-      react: AuthenticationMagicLinkTemplate({
-        userEmail: email,
-        authLink: authLink.toString(),
-      }),
-    })
+    console.log(authLink.toString())
+
+    // await resend.emails.send({
+    //   from: 'Pizza Shop <naoresponda@fala.dev>',
+    //   to: email,
+    //   subject: '[Pizza Shop] Link para login',
+    //   react: AuthenticationMagicLinkTemplate({
+    //     userEmail: email,
+    //     authLink: authLink.toString(),
+    //   }),
+    // })
   },
   {
     body: t.Object({
